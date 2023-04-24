20190fd0812d6f8414e2b33697a5e8bf7b8e17ae
spring <springsquash93@gmail.com>
2023-04-24 16:37:55 -0400

    Add findById Methods

--- src/main/java/me/dev/TodoListWebApp/db/TodoRepository.java
+++ src/main/java/me/dev/TodoListWebApp/db/TodoRepository.java
@@ -4,4 +4,7 @@ import me.dev.TodoListWebApp.models.Todo;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TodoRepository extends CrudRepository<Todo, String>{
+
+    Todo findTodoById(String todoId);
+
 }
