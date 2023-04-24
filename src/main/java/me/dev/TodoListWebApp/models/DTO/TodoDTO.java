213237c971b50033573069e199f02c8613662be8
spring <springsquash93@gmail.com>
2023-04-24 16:40:47 -0400

    Add Todo DTO

---
+++ src/main/java/me/dev/TodoListWebApp/models/DTO/TodoDTO.java
@@ -0,0 +1,38 @@
+package me.dev.TodoListWebApp.models.DTO;
+
+
+public class TodoDTO {
+
+    private String date;
+    private String userId;
+    private String content;
+
+
+    public TodoDTO(){
+
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
