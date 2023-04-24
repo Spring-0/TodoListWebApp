07217c2407fea2bb3bccda0076503f3074ab2fde
spring <springsquash93@gmail.com>
2023-04-23 23:27:05 -0400

    init commit

---
+++ src/test/java/me/dev/TodoListWebApp/TodoListWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dev.TodoListWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
