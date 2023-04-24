cb045280fb4afc8d419f0bf0e5fd48214144a106
spring <springsquash93@gmail.com>
2023-04-24 13:54:19 -0400

    Add reminder and Todo Models

--- src/main/java/me/dev/TodoListWebApp/models/Reminder.java
+++ src/main/java/me/dev/TodoListWebApp/models/Reminder.java
@@ -1,4 +1,17 @@
 package me.dev.TodoListWebApp.models;
 
+import me.dev.TodoListWebApp.constants.DeliveryType;
+import java.sql.Date;
+
+
 public class Reminder {
+
+    private Date data;
+    private String message;
+    private DeliveryType deliveryType;
+
+    public Reminder(Date date, String message, DeliveryType deliveryType){
+
+    }
+
 }
--- src/main/java/me/dev/TodoListWebApp/models/Todo.java
+++ src/main/java/me/dev/TodoListWebApp/models/Todo.java
@@ -1,4 +1,62 @@
 package me.dev.TodoListWebApp.models;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
 public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private Date date;
+
+    @OneToOne
+    private User user;
+
+    private String content;
+
+    public Todo(Date date, User user, String content){
+        this.date = date;
+        this.user = user;
+        this.content = content;
+    }
+
+    public Todo(){
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
