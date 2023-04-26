87b2195036cb88c4e97a2a7e65d939d5c91e4504
spring <springsquash93@gmail.com>
2023-04-25 20:03:29 -0400

    Add scanBasePackages attribute

--- src/main/java/me/dev/TodoListWebApp/TodoListWebAppApplication.java
+++ src/main/java/me/dev/TodoListWebApp/TodoListWebAppApplication.java
@@ -3,7 +3,7 @@ package me.dev.TodoListWebApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "me.dev.TodoListWebApp")
 public class TodoListWebAppApplication {
 
 	public static void main(String[] args) {
