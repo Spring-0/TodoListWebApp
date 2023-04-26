6dbfafdf4f40a80fcb862d72d147778e2b09f51c
spring <springsquash93@gmail.com>
2023-04-25 20:01:44 -0400

    Add view controller for /register, /login, and /index

---
+++ src/main/java/me/dev/TodoListWebApp/controllers/ViewController.java
@@ -0,0 +1,27 @@
+package me.dev.TodoListWebApp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class ViewController {
+
+    @GetMapping("/register")
+    public String viewRegister(){
+        return "register.html";
+    }
+
+
+    @GetMapping("/login")
+    public String viewLogin(){
+        return "login.html";
+    }
+
+
+    @GetMapping("/index")
+    public String viewIndex(){
+        return "index.html";
+    }
+
+}
