8c34d2c7f89109bfe0028b3dae2ebcef9d647263
Wakuma <kingbian72@gmail.com>
2023-04-29 22:57:52 -0400

    get cookie

--- src/main/resources/static/JS/render.js
+++ src/main/resources/static/JS/render.js
@@ -1,50 +1,40 @@
-// test data
-const testUserData = [{
-    id: "221ed967-d6db-485b-99b9-541f84397677", date: "2020-02-02T06:11:11.000+00:00", user: {
-        id: "9430bf7a-6573-4680-96b1-8339d0c85e6f", username: "king", password: "king",
-    }, content: "wake up",
-}, {
-    id: "221ed967-d6db-485b-99b9-541f84397677", date: "2020-02-02T06:11:11.000+00:00", user: {
-        id: "9430bf7a-6573-4680-96b1-8339d0c85e6f", username: "king", password: "king",
-    }, content: "wake up",
-}, {
-    id: "221ed967-d6db-485b-99b9-541f84397677", date: "2020-02-02T06:11:11.000+00:00", user: {
-        id: "9430bf7a-6573-4680-96b1-8339d0c85e6f", username: "king", password: "king",
-    }, content: "wake up",
-}, // Add more todo objects as needed...
-];
 
-function getID(event) {
+/*function getID(event) {
 
     event.preventDefault()
     // grab the input from the user regarding todo id ??
     const input = document.getElementById("userIdInput")
 
     const id = input.value;
-    loadTodos(id)
+    //loadTodos(id)
     console.log(id)
 
+}*/
+
+function getCookies() {
+    const cookieValue = document.cookie.split(";").
+    find((row) => row.startsWith("userId")).split("=")[1];
+
+    console.log(cookieValue)
+     return cookieValue
 }
 
-async function loadTodos(userId) {
+loadTodos(getCookies())
+ function loadTodos(userId) {
 
-    fetch("/todo/get?userId=" +userId, {
+    fetch("/todo/get?userId=" + userId, {
         method: 'GET', header: {
             'Content-Type': 'Application/Json'
         },
     })
-         .then(response  => {
+        .then(response => {
             if (response.status === 200) {
                 //console.log("theres data coming through")
-                console.log(userId +" dsdsds ssds1 success")
-
                 createTodoTable(response.json())
-                console.log(userId +" dsdsds ssds1 2222 success")
 
-        } else {
+            } else {
                 // Show Error
                 alert("Unable to load Todo's")
-                console.log(userId + " 111111 waderror")
             }
         })
 }
@@ -52,20 +42,20 @@ async function loadTodos(userId) {
 
 function createTodoTable(todos) {
 
-    todos.then(response  =>{
-    const table = document.getElementById("myTable")
+    todos.then(response => {
+        const table = document.getElementById("myTable")
 
-    for (let i = 0; i < response.length; i++) {
+        for (let i = 0; i < response.length; i++) {
 
-        const row = `<tr>
+            const row = `<tr>
                                 <td>${response[i].user.username}</td>
                                 <td> ${response[i].content}</td>
                                 <td> ${response[i].date.t}</td>
                                 <td> <input  type="checkbox"></td>
                           </tr> `;
-        table.innerHTML += row
+            table.innerHTML += row
 
-    }
+        }
     });
 
 }
